7140c25f9ed6d88599b84cbe0c52ce3c640f79b5
Gerardo O <goaccess@prosoftcorp.com>
2021-02-04 19:22:59 -0600

    Added custom indentation.

--- xembed.c
+++ xembed.c
@@ -8,38 +8,37 @@
 #include <unistd.h>
 
 int
-main(int argc, char *argv[])
-{
-	char *xembed;
-	int tty;
-	pid_t pgrp, tcpgrp;
+main (int argc, char *argv[]) {
+  char *xembed;
+  int tty;
+  pid_t pgrp, tcpgrp;
 
-	if (argc < 3) {
-		fprintf(stderr, "usage: %s flag cmd ...\n", argv[0]);
-		return 2;
-	}
+  if (argc < 3) {
+    fprintf (stderr, "usage: %s flag cmd ...\n", argv[0]);
+    return 2;
+  }
 
-	if (!(xembed = getenv("XEMBED")))
-		goto noembed;
+  if (!(xembed = getenv ("XEMBED")))
+    goto noembed;
 
-	if ((tty = open("/dev/tty", O_RDONLY)) < 0)
-		goto noembed;
+  if ((tty = open ("/dev/tty", O_RDONLY)) < 0)
+    goto noembed;
 
-	pgrp = getpgrp();
-	tcpgrp = tcgetpgrp(tty);
+  pgrp = getpgrp ();
+  tcpgrp = tcgetpgrp (tty);
 
-	close(tty);
+  close (tty);
 
-	if (pgrp == tcpgrp) { /* in foreground of tty */
-		argv[0] = argv[2];
-		argv[2] = xembed;
-	} else {
-noembed:
-		argv += 2;
-	}
+  if (pgrp == tcpgrp) { /* in foreground of tty */
+    argv[0] = argv[2];
+    argv[2] = xembed;
+  } else {
+  noembed:
+    argv += 2;
+  }
 
-	execvp(argv[0], argv);
+  execvp (argv[0], argv);
 
-	perror(argv[0]); /* failed to execute */
-	return 1;
+  perror (argv[0]);     /* failed to execute */
+  return 1;
 }
